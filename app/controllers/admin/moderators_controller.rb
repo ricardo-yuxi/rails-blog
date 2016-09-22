7315eead60539a51e3c1c70a96f4376b6de950fc
Ricardo Rojas <Richi@Ricardos-MacBook-Pro.local>
2016-09-22 11:17:17 -0500

    add moderator index, edit, update

---
+++ app/controllers/admin/moderators_controller.rb
@@ -0,0 +1,27 @@
+class Admin::ModeratorsController < ApplicationController
+  def index
+    @moderators = Moderator.all
+  end
+
+  def edit
+    @moderator = Moderator.find(params[:id])
+  end
+
+  def update
+    @moderator = Moderator.find(params[:id])
+
+    if @moderator.update(moderator_params)
+      flash[:notice] = "Moderator was successfully created."
+      redirect_to admin_moderators_url
+    else
+      flash[:alert] = "There was a problem updating moderator."
+      render 'edit'
+    end
+  end
+
+  private
+
+  def moderator_params
+    params.require(:moderator).permit(:id, :fullname, :username, :password)
+  end
+end
--- app/models/moderator.rb
+++ app/models/moderator.rb
@@ -2,4 +2,8 @@ class Moderator < ApplicationRecord
   has_secure_password
 
   has_many :posts
+
+  validates :fullname, presence: true
+  validates :username, presence: true
+  validates :password, presence: true
 end
